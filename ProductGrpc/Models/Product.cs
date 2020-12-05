a36311caa7c0d0ad3bf9c3d3de1817d43a0b0b48
Abdurrahman Köken <abdurrahman@akoken.com>
2020-12-05 23:55:35 +0300

    initial commit.

---
+++ ProductGrpc/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ProductGrpc.Models
+{
+    public class Product
+    {
+
+        public int ProductId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public ProductStatus Status { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+
+    public enum ProductStatus
+    {
+        None = 0,
+
+        InStock = 1,
+
+        Low = 2
+    }
+}
