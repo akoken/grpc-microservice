d18fe270ad70624f864d6e00c75e61f89d8838ca
Abdurrahman Köken <abdurrahman@akoken.com>
2020-12-19 22:03:42 +0300

    remove unnecessary namespaces.

--- src/ProductGrpc/Enumerations/ProductStatus.cs
+++ src/ProductGrpc/Enumerations/ProductStatus.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace ProductGrpc.Enumerations
+﻿namespace ProductGrpc.Enumerations
 {
     public enum ProductStatus
     {
