2d944c8a369ae7cb17b8ed073bb2988f4719db67
Abdurrahman Köken <abdurrahman@akoken.com>
2020-12-24 23:44:39 +0300

    consume product grpc service from worker service.

--- src/ProductWorkerService/Worker.cs
+++ src/ProductWorkerService/Worker.cs
@@ -1,6 +1,8 @@
+using Grpc.Net.Client;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ProductGrpc.Protos;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -20,9 +22,24 @@ namespace ProductWorkerService
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            Console.WriteLine("Waiting for server is running.");
+            Thread.Sleep(3000);
+
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+
+                using var channel = GrpcChannel.ForAddress(_config.GetValue<string>("WorkerService:ServiceUrl"));
+                var client = new ProductProtoService.ProductProtoServiceClient(channel);
+
+                Console.WriteLine("GetProductAsync started...");
+                var response = await client.GetProductAsync(new GetProductRequest
+                {
+                    ProductId = 1
+                });
+
+                Console.WriteLine("GetProductAsync response: " + response.ToString());
+
                 await Task.Delay(_config.GetValue<int>("WorkerService:TaskInterval"), stoppingToken);
             }
         }
