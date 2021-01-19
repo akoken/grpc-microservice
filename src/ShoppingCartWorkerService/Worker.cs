b6d6b82995c820c3011bf94b790006b6fd8145b6
akoken <akoken@outlook.com>
2021-01-19 22:44:15 +0300

    * .DS_Store: add products in shopping cart grpc worker service.
    * .DS_Store:
    * Worker.cs:
    * appsettings.json:
    * ShoppingCartWorkerService.csproj:

--- src/ShoppingCartWorkerService/Worker.cs
+++ src/ShoppingCartWorkerService/Worker.cs
@@ -1,29 +1,102 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Net.Client;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ProductGrpc.Protos;
+using ShoppingCartGrpc.Protos;
 
 namespace ShoppingCartWorkerService
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly IConfiguration _config;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, IConfiguration configuration)
         {
-            _logger = logger;
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _config = configuration ?? throw new ArgumentNullException(nameof(configuration));
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            Console.WriteLine("Waiting for server is running ...");
+            Thread.Sleep(2000);
+             
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+
+                using var scChannel = GrpcChannel.ForAddress(_config.GetValue<string>("WorkerService:ShoppingCartServerUrl"));
+                var scClient = new ShoppingCartProtoService.ShoppingCartProtoServiceClient(scChannel);
+
+                var scModel = await GetOrCreateShoppingCartAsync(scClient);
+
+                using var scClientStream = scClient.AddItemToShoppingCart();
+
+                using var productChannel = GrpcChannel.ForAddress(_config.GetValue<string>("WorkerService:ProductServerUrl"));
+                var productClient = new ProductProtoService.ProductProtoServiceClient(productChannel);
+
+                _logger.LogInformation("GetAllProducts started...");
+                using var clientData = productClient.GetAllProducts(new GetAllProductsRequest());
+                await foreach (var responseData in clientData.ResponseStream.ReadAllAsync())
+                {
+                    _logger.LogInformation("GetAllProducts Stream Response: {responseData}", responseData);
+
+                    var newScItem = new AddItemToShoppingCartRequest
+                    {
+                        Username = _config.GetValue<string>("WorkerService:UserName"),
+                        DiscountCode = "CODE_100",
+                        NewCartItem = new ShoppingCartItemModel
+                        {
+                            ProductId = responseData.ProductId,
+                            ProductName = responseData.Name,
+                            Price = responseData.Price,
+                            Color = "Black",
+                            Quantity = 1
+                        }
+                    };
+
+                    await scClientStream.RequestStream.WriteAsync(newScItem);
+                    _logger.LogInformation("ShoppingCart Client Stream Added New Item : {addNewScItem}", newScItem);
+                }
+
+                await scClientStream.RequestStream.CompleteAsync();
+                var addItemToShoppingCartResponse = await scClientStream;
+                _logger.LogInformation("AddItemToShoppingCart Clien Stream Response : {addItemToShoppingCartResponse}", addItemToShoppingCartResponse);
+
+                await Task.Delay(_config.GetValue<int>("WorkerService:TaskInterval"), stoppingToken);
             }
+        }
+
+        private async Task<ShoppingCartModel> GetOrCreateShoppingCartAsync(ShoppingCartProtoService.ShoppingCartProtoServiceClient scClient)
+        {
+            ShoppingCartModel shoppingCartModel;
+
+            try
+            {
+                _logger.LogInformation("GetShoppingCartAsync started..");
+                shoppingCartModel = await scClient.GetShoppingCartAsync(new GetShoppingCartRequest { Username = _config.GetValue<string>("WorkerService:UserName") });
+                _logger.LogInformation("GetShoppingCartAsync Response: {shoppingCartModel}", shoppingCartModel);
+            }
+            catch (RpcException exception)
+            {
+                if(exception.StatusCode == StatusCode.NotFound)
+                {
+                    _logger.LogInformation("CreateShoppingCartAsync started..");
+                    shoppingCartModel = await scClient.CreateShoppingCartAsync(new ShoppingCartModel { Username = _config.GetValue<string>("WorkerService:UserName") });
+                    _logger.LogInformation("CreateShoppingCartAsync Response: {shoppingCartModel}", shoppingCartModel);
+                }
+                else
+                {
+                    throw; 
+                }
+            }
+            return shoppingCartModel;
         }
     }
 }
